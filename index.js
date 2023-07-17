cdf5aedbaa584d8b3e44a2d426eb308f06ae3e26
dingyi222666 <dingyi222666@foxmail.com>
2023-07-17 19:58:15 +0800

    feat: add build file

---
+++ index.js
@@ -0,0 +1,25 @@
+import { load } from "js-yaml"
+import fs from "fs/promises"
+
+async function main() {
+    const presetFiles = await fs.readdir("presets")
+
+    const output = []
+    for (const presetFile of presetFiles) {
+        const preset = await fs.readFile(`presets/${presetFile}`, "utf-8")
+        const presetData = load(preset)
+
+        output.push({
+            keywords: presetData.keywords,
+            name: presetFile.replace(".yml", ""),
+            rawPath: `https://raw.githubusercontent.com/ChatHubLab/awesome-chathub-presets/main/presets/${presetFile}`,
+        })
+    }
+
+    await fs.writeFile("presets.json", JSON.stringify(output, null, 2))
+}
+
+
+(async function () {
+    await main()
+})()
